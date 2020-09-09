0d2785f9155f1b5f10250dc6742b8f49ec16b419
konrad <sochacki.konrad@gmail.com>
2020-09-09 16:35:10 +0200

    Put tests in function

--- run_tests.sh
+++ run_tests.sh
@@ -1,19 +1,38 @@
-make clean ;
-make &&
-./pascal-compiler testing/exp_pas/t0.pas testing/mine_out/t0.asm &&
+#!/bin/bash
+function run_test () {
+  arr=("$@")
+#  printf "%s\n" "${arr[@]}"
 
-echo "\nTests:"
-cd testing
-#t0="$(cat mine_out/t0.asm | awk -F';' '{print $1}' | tr -d '[:space:]')"
-mt0=`cat mine_out/t0.asm | awk -F';' '{print $1}' | tr -d '[:space:]'`
-#echo $mt0
-gt0=`cat gj_out/t0.asm | awk -F';' '{print $1}' | tr -d '[:space:]'`
-#echo $gt0
-if [ $mt0 = $gt0 ]; then
-  echo t0 correct
-else
-  echo t0 not correct!
-fi
+  for prog in "${arr[@]}";
+  do
+#    echo $prog
+    ./pascal-compiler testing/exp_pas/$prog.pas testing/mine_out/$prog.asm &&
+      #t0="$(cat mine_out/t0.asm | awk -F';' '{print $1}' | tr -d '[:space:]')"
+    mt0=`cat testing/mine_out/$prog.asm | awk -F';' '{print $1}' | tr -d '[:space:]'`
+    #echo $mt0
+    gt0=`cat testing/gj_out/$prog.asm | awk -F';' '{print $1}' | tr -d '[:space:]'`
+    #echo $gt0
+    if [ $mt0 = $gt0 ]; then
+      echo $prog correct
+    else
+      echo $prog not correct!
+    fi
+  done
+
+
+
+}
+
+programArray=(t0 t1)
+
+make clean ; make
+printf "\nTests:\n"
+
+#printf "%s\n" "${programArray[@]}"
+#cd testing
+run_test "${programArray[@]}"
+
+
 
 
 #cat t0.asm
